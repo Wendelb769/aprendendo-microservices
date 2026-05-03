da17642849f762f6e84afc433ed7617610c79a53
Wendel Batista <wendelbatista769@gmail.com>
2026-05-03 05:11:50 -0300

    Correção de emailId e userId em EmailModel

--- email/src/main/java/com/example/email/domain/EmailModel.java
+++ email/src/main/java/com/example/email/domain/EmailModel.java
@@ -1,14 +1,13 @@
 package com.example.email.domain;
 
 import com.example.email.enums.EmailStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
@@ -16,8 +15,13 @@ import java.time.LocalDateTime;
 @Table(name = "TB_EMAIL")
 @Data
 public class EmailModel {
-    private String emailId;
-    private String userId;
+
+    private final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+    private UUID userId;
     private String emailTo;
     private String emailFrom;
     private String emailSubject;
